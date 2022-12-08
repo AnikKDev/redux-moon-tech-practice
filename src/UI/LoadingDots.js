8a84f77d20b402747ce1d2e5ac4fe52e2e1feaf0
anikdev2016 <anikdev2016@gmail.com>
2022-12-08 11:48:32 +0600

    completed top rated component

---
+++ src/UI/LoadingDots.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./LoadingDots.css";
+const LoadingDots = () => {
+  return (
+    <>
+      <div class="dots-1"></div>
+      <div class="dots-2"></div>
+      <div class="dots-3"></div>
+      <div class="dots-4"></div>
+      <div class="dots-5"></div>
+      <div class="dots-6"></div>
+      <div class="dots-7"></div>
+      <div class="dots-8"></div>
+      <div class="dots-9"></div>
+      <div class="dots-10"></div>
+    </>
+  );
+};
+
+export default LoadingDots;
--- src/pages/TopRated.js
+++ src/pages/TopRated.js
@@ -1,9 +1,30 @@
 import React from "react";
+import ProductCard from "../components/ProductCard";
+import useProducts from "../context/hook/useProducts";
+import LoadingDots from "../UI/LoadingDots";
 
 const TopRated = () => {
+  const { state } = useProducts();
+  const { products, loading, error } = state;
+
+  let content;
+  if (loading) {
+    content = <LoadingDots />;
+  }
+  if (error) {
+    content = <p>Error</p>;
+  }
+  if (!loading && !error && products.length === 0) {
+    content = <p>Nothing to show! product is empty</p>;
+  }
+  if (!loading && !error && products.length) {
+    content = products
+      .filter((product) => product.rating >= 4)
+      .map((item) => <ProductCard product={item} key={item._id} />);
+  }
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
-      <h1>This is featured page</h1>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10">
+      {content}
     </div>
   );
 };
